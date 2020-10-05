47b4e17501e05ec99835294d77dd79d325fd5925
rafaelghesti <66754636+rafaelghesti@users.noreply.github.com>
2020-10-05 17:59:28 -0300

    Adicionar arquivos de projeto.

---
+++ CompositionPratics/Entities/Departament.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompositionPratics.Entities
+{
+    class Departament
+    {
+        public string Name { get; set; }
+
+        public Departament()
+        {
+
+        }
+
+        public Departament(string name)
+        {
+            Name = name;
+        }
+
+    }
+}
---
+++ CompositionPratics/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompositionPratics.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior,
+        Midlevel,
+        Senior
+    }
+}
